6672efbf05c7c083862b55068536b521b87294d1
Clément Janssens <clement.janssens.ti@gmail.com>
2019-12-21 12:15:37 +0100

    Step 3

--- app/src/main/java/com/example/combee/Activity/BusinessActivity.java
+++ app/src/main/java/com/example/combee/Activity/BusinessActivity.java
@@ -1,4 +1,4 @@
-package com.example.combee.Activity;
+package com.example.combee.activity;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
--- app/src/main/java/com/example/combee/Activity/BusinessListActivity.java
+++ app/src/main/java/com/example/combee/Activity/BusinessListActivity.java
@@ -1,4 +1,4 @@
-package com.example.combee.Activity;
+package com.example.combee.activity;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
